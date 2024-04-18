8c8290f54a96c475bd5d0c11bdd069dade508a48
Hagu Bian <132670995+bian-hagu@users.noreply.github.com>
2024-04-18 16:09:39 +0700

    Add editor mode,& Completed jump funtion (#2)
    * Change code organize, re-collision processing
    
    * Create README.md
    
    * complete collision detection & basic movement
    
    * change file, optimize load map
    
    * add Animation
    
    * add map editor mode
    
    * add scroll funtion for editor mode
    
    * Change jump juntion & Add double jump

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
-from scripts.utils import load_img
-from scripts.entities import Entity
+from scripts.utils import *
+from scripts.entities import Entity, Player
 from scripts.tilemap import Tilemap
 class Game:
   def __init__(self):
@@ -14,37 +14,53 @@ class Game:
     self.clock = pygame.time.Clock()
     self.movement = [False, False]
     self.assets = {
-      'player': load_img('herochar.png', (50, 50)),
-      'grass_top': load_img('grass_top.png', (50, 50)), 
-      'stone': load_img('stone.png', (50, 50)),
-      'background': load_img('background.png', (1280,720))
+      'grass': load_imgs('tiles/grass'), 
+      'background': load_img('background/background.png', (1280,720)),
+      'background1': load_img('background/bg.png', (1280,720)),
+      'player/idle': Animation(load_imgs('entities/hero/hero_idle'), duration=4), 
+      'player/jump_up': Animation(load_imgs('entities/hero/hero_jump_up'), duration = 3),
+      'player/jump_down': Animation(load_imgs('entities/hero/hero_jump_down'), duration = 3),
+      'player/run' : Animation(load_imgs('entities/hero/hero_run'), duration= 6),
+      'player/jump_double': Animation(load_imgs('entities/hero/hero_jump_double'), duration = 6),
     }
 
-    self.player = Entity(self, 'player', (50, 450), (50, 50), 5)
+    self.player = Player(self, (50, 450), (50, 50))
     self.tilemap = Tilemap(self, size=50)
     self.scroll = [0,0]
+    try:
+      self.tilemap.load('map.json')
+    except:
 
-  def run(self):  
+      # raise FileNotFoundError('Map file not found')
+      pass
+  def run(self):    
     while True:
       self.display.blit(self.assets['background'], (0,0))
       if self.player.pos[0] > self.display.get_width()/2:
         self.scroll[0] += (self.player.rect().centerx - self.display.get_width()/2 - self.scroll[0])
-      self.scroll[1] += (self.player.rect().centery - self.display.get_height()/2 - self.scroll[1])
-      
+      if self.player.pos[1] < 300:
+        self.scroll[1] += (self.player.rect().centery - self.display.get_height()/2 - self.scroll[1])
+      if self.player.pos[1] > 600:
+        self.scroll[1] += (self.player.rect().centery - self.display.get_height()/2 - self.scroll[1])   
       render_scroll = ((self.scroll[0], (self.scroll[1])))    
       self.tilemap.render(self.display, offset=render_scroll)
       self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-      self.player.render(self.display,  self.movement, offset=render_scroll)    
-
+      self.player.render(self.display, offset=render_scroll)    
+      # if self.player.pos[1] > 1000:
+      #   print("Game over")
+      #   pygame.quit()
+      #   sys.exit()
       keys = pygame.key.get_pressed()
       for event in pygame.event.get():  
         if event.type == pygame.QUIT:
-          pygame.quit()
-          sys.exit()
+          pygame.quit() 
+          sys.exit()  
         if event.type == pygame.KEYDOWN:
           if event.key == pygame.K_ESCAPE:
             pygame.quit()
             sys.exit()
+          if event.key == pygame.K_SPACE:
+            self.player.jump()
           if event.key == pygame.K_a or event.key == pygame.K_LEFT:
             self.movement[0] = True
           if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
@@ -54,11 +70,11 @@ class Game:
             self.movement[0] = False
           if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
             self.movement[1] = False
-      if keys[pygame.K_SPACE] and self.player.collision['bottom']:
-        self.player.velocity[1] = -20
+        
 
       self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
       pygame.display.update()
       self.clock.tick(60)
+
 
 Game().run()
--- scripts/entities.py
+++ scripts/entities.py
@@ -2,6 +2,27 @@ import pygame
 GRAVITY = 10
 class Entity:
   def __init__(self, game, type, pos, size, speed=5):
+    """
+    Initialize a new Entity instance.
+
+    Parameters
+    ----------
+    game : Game instance
+      The game instance that the entity belongs to.
+    type : str
+      The type of the entity, used to look up its assets.
+    pos : tuple of int
+      The (x, y) position of the entity.
+    size : tuple of int
+      The (width, height) size of the entity.
+    speed : int, optional
+      The movement speed of the entity, default to 5.
+
+    Returns
+    -------
+    None
+
+    """
     self.game = game
     self.type = type
     self.pos = list(pos)
@@ -9,11 +30,55 @@ class Entity:
     self.velocity = [0,0]
     self.speed = speed
     self.collision = {'top': False, 'bottom': False, 'left': False, 'right': False}
+    
+    self.action = ''
+    self.animation_offset = (-3,-3)
+    self.flip = False
+    self.set_action('idle')
+
+
 
   def rect(self):
+    """
+    Returns the rectangular bounding box of the entity.
+
+    Returns
+    -------
+    pygame.Rect
+      The rectangular bounding box of the entity.
+    """
     return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
 
+  def set_action(self, action):
+    """Set the current action of the entity.
+
+    Parameters
+    ----------
+    action (str): The name of the action to set.
+
+    Returns
+    -------
+      None
+    """
+    if action != self.action:
+      self.action = action
+      self.animation = self.game.assets[self.type + '/' + self.action].copy()
+  
   def update(self, tilemap, movement = (0, 0)):
+    """
+    Update the position and state of the entity.
+
+    Parameters
+    ----------
+    tilemap : Tilemap instance
+        The tilemap instance that the entity is moving on.
+    movement : tuple of int, optional
+        The (x, y) movement vector of the entity, default to (0, 0).
+
+    Returns
+    -------
+    None
+    """
     self.collision = {'top': False, 'bottom': False, 'left': False, 'right': False}
     movement = (self.velocity[0] + movement[0], self.velocity[1] + movement[1])
     
@@ -43,17 +108,74 @@ class Entity:
           entity_rect.top = rect.bottom
           self.collision['top'] = True      
       self.pos[1] = entity_rect.y
+    
+    # Update animation ---------------------------------------------------------------------
+    if movement[0] > 0:
+      self.flip = False
+    if movement[0] < 0:
+      self.flip = True
 
     # Update velocity -------------------------------------------------------------------------
     self.velocity[1] = min(GRAVITY, self.velocity[1] +1)
     if self.collision['bottom'] or self.collision['top']:
       self.velocity[1] = 0
+    self.animation.update()
 
 
 
-  def render(self, surf, direct, offset = (0, 0)):
-    asset = self.game.assets['player']
-    if direct[0] :
-       asset = pygame.transform.flip(asset, True, False)
-    surf.blit(asset, (self.pos[0] - offset[0], self.pos[1] - offset[1]))
-    # pygame.draw.rect(surf, (255, 0, 0), self.rect(), 1)
+  def render(self, surf, offset = (0, 0)):
+    asset = pygame.transform.flip(self.animation.img(), self.flip, False)
+    surf.blit(asset, (self.pos[0] - offset[0] + self.animation_offset[0], self.pos[1] - offset[1] + self.animation_offset[1] ))
+
+
+
+
+
+
+
+
+
+
+
+
+class Player(Entity):
+  def __init__(self, game, pos, size):
+    super().__init__(game, 'player', pos, size, 5)
+    self.air_time = 0
+    self.jumps = 1
+    self.doublejumps_cooldown = 0
+
+  def update(self, tilemap, movement=(0, 0)):
+    super().update(tilemap, movement)
+    
+    self.air_time += 1
+
+    if self.collision['bottom']:
+      if self.jumps <2:
+        self.jumps += 1
+      self.air_time = 0
+      self.doublejumps_cooldown -=1
+
+    if self.air_time > 1 and self.jumps == 0 and self.velocity[1] < 5:
+      self.set_action('jump_double')
+    elif self.air_time > 1 and self.velocity[1] < 0:
+      self.set_action('jump_up')
+    elif self.air_time > 10 and self.velocity[1] > 0:
+      self.set_action('jump_down')
+    elif movement[0] != 0:
+      self.set_action('run')
+    else:
+      self.set_action('idle')
+    print(self.doublejumps_cooldown)
+
+  def jump(self):
+    if self.jumps == 2:
+      self.velocity[1] -= 15
+      self.jumps -= 1
+    elif self.jumps == 1 and self.doublejumps_cooldown <= 0:   
+      self.velocity[1] -= 15
+      self.jumps -= 1
+      self.doublejumps_cooldown = 60
+
+
+
