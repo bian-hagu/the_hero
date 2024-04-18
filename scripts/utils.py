474398da123e97cd533c56e4ba5ea035c5d178b9
Hagu Bian <132670995+bian-hagu@users.noreply.github.com>
2024-04-18 16:17:55 +0700

    Fix merge #2 (#4)
    * Change code organize, re-collision processing
    
    * Create README.md
    
    * complete collision detection & basic movement
    
    * change file, optimize load map
    
    * add Animation
    
    * add map editor mode
    
    * add scroll funtion for editor mode
    
    * Change jump juntion & Add double jump
    
    * fix bugs

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,39 @@
 import pygame 
+import os
+BASE_IMG_PATH = 'asset/imgs/'
+DEFFAULT_SIZE = (50,50)
+def load_img(path, size = DEFFAULT_SIZE):
+  imgs =pygame.image.load(BASE_IMG_PATH + path).convert()
+  img = pygame.transform.scale(imgs, size)
+  img.set_colorkey((0,0,0))
+  return img
 
-BASE_IMG_PATH = 'asset/img/'
 
-def load_img(path, size):
-  return pygame.transform.scale(pygame.image.load(BASE_IMG_PATH + path), size)
+def load_imgs(path):
+  imgs = []
+  for imgname in sorted(os.listdir(BASE_IMG_PATH + path)):
+    imgs.append(load_img(path + '/' + imgname))
+  return imgs
+
+class Animation:
+  def __init__(self, img, duration, loop=True):
+    self.imgs = img
+    self.loop = loop
+    self.duration = duration
+    self.done = False
+    self.frame = 0
+
+  def copy(self):
+    return Animation(self.imgs, self.duration, self.loop)
+
+  def update(self):
+    if self.loop:
+      self.frame  = (self.frame + 1) % (self.duration * len(self.imgs))
+    else:
+      maxframe = self.duration * len(self.imgs) -1
+      self.frame = min(self.frame + 1, maxframe)
+      if self.frame >= maxframe:
+        self.done = True
+
+  def img(self):
+    return self.imgs[int(self.frame / self.duration)]
